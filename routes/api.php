57b4c5bff1f404df9cae8d7f5c7c0b8d270bdeff
Richard Litz <richardlitz@gmail.com>
2025-04-17 11:24:54 -0300

    rotas

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\CommentController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    // Task routes
+    Route::get('/buildings/{building}/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    
+    // Comment routes
+    Route::post('/tasks/{task}/comments', [CommentController::class, 'store']);
 });
